2036930a7ab2d2cd89b615d469c23f5aa4b23935
ikismail <ikismail7@gmail.com>
2018-10-06 20:36:11 +0530

    Separated Create Account component, Implemented Login functionalities

---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="create-account">
+        <form class="form-signup">
+                    <img class="mb-4" src="../assets/create-account.svg" alt="" width="72" height="72">
+                    <h1 class="h3 mb-3 font-weight-normal">Create an account</h1>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <input type="text" class="form-control" id="firstName" placeholder="First Name" value="" required>
+                            <div class="invalid-feedback">
+                            Valid first name is required.
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <input type="text" class="form-control" id="lastName" placeholder="Last Name" value="" required>
+                            <div class="invalid-feedback">
+                            Valid last name is required.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md mb-3">
+                            <input type="text" class="form-control" id="emailId" placeholder="Email address" value="" required>
+                            <div class="invalid-feedback">
+                            Valid first name is required.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md mb-3">
+                            <input type="text" class="form-control" id="act-password" placeholder="New password" value="" required>
+                            <div class="invalid-feedback">
+                            Valid first name is required.
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+                    <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+                </form>    
+    </div>    
+</template>
+<script>
+export default {
+    name: "CreateAccount"
+}
+</script>
+
+<style>
+.form-signup {
+  width: 100%;
+  max-width: 500px;
+  padding: 15px;
+  margin: auto;
+}
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -3,54 +3,18 @@
 
         <div class="row">
             <div class="col">
-                <form class="form-signin">
+                <form class="form-signin" @submit.prevent="login">
                     <img class="mb-4" src="../assets/logo.png" alt="" width="72" height="72">
                     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                    <label for="inputEmail" class="sr-only" >Email address</label>
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autocomplete="email">
-                    <label for="inputPassword" class="sr-only">Password</label>
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required autocomplete="current-password">
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
+                    <label for="email" class="sr-only" >Email address</label>
+                    <input type="email" id="email" class="form-control" v-model="email" placeholder="Email address" required autocomplete="email">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" id="password" v-model="password" class="form-control" placeholder="Password" required autocomplete="current-password">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit" > <i class="fa fa-spinner fa-spin mr-1" v-if="showLoader"></i> Log In</button>
                 </form>
             </div>
             <div class="col">
-                <form class="form-signup">
-                    <img class="mb-4" src="../assets/create-account.svg" alt="" width="72" height="72">
-                    <h1 class="h3 mb-3 font-weight-normal">Create an account</h1>
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <input type="text" class="form-control" id="firstName" placeholder="First Name" value="" required>
-                            <div class="invalid-feedback">
-                            Valid first name is required.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <input type="text" class="form-control" id="lastName" placeholder="Last Name" value="" required>
-                            <div class="invalid-feedback">
-                            Valid last name is required.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md mb-3">
-                            <input type="text" class="form-control" id="emailId" placeholder="Email address" value="" required>
-                            <div class="invalid-feedback">
-                            Valid first name is required.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md mb-3">
-                            <input type="text" class="form-control" id="password" placeholder="New password" value="" required>
-                            <div class="invalid-feedback">
-                            Valid first name is required.
-                            </div>
-                        </div>
-                    </div>
-
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
-                    <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-                </form>
+                <createAccount></createAccount>
             </div>
         </div>
          
@@ -58,26 +22,29 @@
     </div>    
 </template>
 <script>
+import CreateAccount from "../components/CreateAccount";
 export default {
-  name: "login"
+  name: "login",
+  components: { CreateAccount },
+  data() {
+    return {
+      email: "",
+      password: "",
+      showLoader: false
+    };
+  },
+  methods: {
+    //   Login function usign email and password
+    login(event) {
+        this.showLoader = true;
+      console.log(this.email, this.password);
+      event.target.reset();
+       this.showLoader = false;
+    }
+  }
 };
 </script>
 <style>
-/* html,
-body {
-  height: 100%;
-}
-
-body {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
-} */
-
 .login {
   position: relative;
   top: 50px;
@@ -89,12 +56,7 @@ body {
   padding: 15px;
   margin: auto;
 }
-.form-signup {
-  width: 100%;
-  max-width: 500px;
-  padding: 15px;
-  margin: auto;
-}
+
 .form-signin .checkbox {
   font-weight: 400;
 }
