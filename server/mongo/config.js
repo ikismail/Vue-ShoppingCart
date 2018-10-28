c70494d14c1261c79e16212a9de212a22d037aad
ikismail7 <ikismail7@gmail.com>
2018-10-28 15:34:58 +0530

    Updated Readme file

--- server/mongo/config.js
+++ server/mongo/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://ikismail:qwerty12@ds127362.mlab.com:27362/vue-shop', {
+mongoose.connect('<YOUR_MONGODB_URL>', {
     useNewUrlParser: true
 }); // connect to our database
 
