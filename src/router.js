8432b1d7a71fa675627fbfc3b57677abb81edf7b
ikismail <ikismail7@gmail.com>
2018-10-31 18:01:56 +0530

    Implemented Cart Products and functionalities 🙌🚀🤘🛒

--- src/router.js
+++ src/router.js
@@ -7,8 +7,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
@@ -19,12 +18,17 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/products',
       name: 'products',
       component: () => import('./views/Products.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('./components/CartProducts.vue')
     },
     {
       path: '/login',
