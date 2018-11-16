1a4e6cea293a78a88fc223d6cff567d7eaa064b4
ikismail <ikismail7@gmail.com>
2018-11-17 03:32:13 +0530

    Fixed Router Issue, getting page while reload page

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [{
       path: '/',
