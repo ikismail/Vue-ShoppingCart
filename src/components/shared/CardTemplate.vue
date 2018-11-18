26d486f84ec166afe4362ce13c860fa6fc78803e
ikismail <ikismail7@gmail.com>
2018-11-19 00:17:08 +0530

    Implementing Updating Product with Modal Dialouge 👨‍💻

--- src/components/shared/CardTemplate.vue
+++ src/components/shared/CardTemplate.vue
@@ -11,8 +11,11 @@
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary" 
                         v-on:click="navigateProductDetail(item)">View</button>
+                        
                         <button type="button" 
-                        class="btn btn-sm btn-outline-secondary">Edit</button>
+                        class="btn btn-sm btn-outline-secondary" 
+                        v-on:click="updateEditProduct(item, item._id)"
+                        >Edit</button>
                     </div>
                     <small class="text-muted footerIcons">
                         <a href="javascript:;;" class="p-2"><i class="fa fa-heart"></i></a>
@@ -22,16 +25,24 @@
                 </div>
             </div>
         </div>
+        <edit-product ref="editProduct" />
     </div>
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations } from "vuex";
-import { infoToaster, successToaster } from "./service/ErrorHandler.js";
+import { infoToaster, successToaster } from "./service/ErrorHandler";
+import EditProduct from "../../components/products/actions/EditProduct";
 import _ from "lodash";
 export default {
   name: "cardTemplate",
   props: ["item"],
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  components: { EditProduct },
   methods: {
     navigateProductDetail(product) {
       this.$router.push({
@@ -50,6 +61,10 @@ export default {
         successToaster("Added Successfully", "Product Added Successfully");
         this.ADD_CART_LOCAL(product);
       }
+    },
+
+    updateEditProduct(product, id) {
+      this.$refs.editProduct.setProduct(product);
     }
   },
   mounted() {}
