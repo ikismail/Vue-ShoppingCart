7140e6bd50d40c98c4dda1a5aa4641cadadef2db
ikismail <ikismail7@gmail.com>
2018-11-21 00:58:29 +0530

    Implemented Product Filter as SeparateComponent & done filter by seller

---
+++ src/components/products/filters/ProductFilter.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="productFilter">
+        <h4 class="d-flex mb-3">
+            <span class="text-muted">Filters: </span>
+          </h4>
+          <div class="form-group">
+                <label for="filterPrdCty">By Category:</label>
+                <select class="form-control" id="filterPrdCty" v-model="selectedCategory" @change="(event) => updateProductCategory(event, 'productCategory')">
+                <option 
+                v-for="(category, index) in categories" :key="index" 
+                v-bind:value="category.productCategory" 
+                >{{category.productCategory}}</option>
+                </select>
+          </div>
+          <div class="form-group">
+                <label for="filterPrdSlr">By Seller:</label>
+                <select class="form-control" id="filterPrdSlr" v-model="selectedSeller" @change="(event) => updateProductCategory(event, 'productSeller')">
+                <option 
+                v-for="(seller, index) in sellers" :key="index" 
+                v-bind:value="seller.productSeller" 
+                >{{seller.productSeller}}</option>
+                </select>
+          </div>
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "productFilter",
+    props: ['categories', 'sellers'],
+    data() {
+        return {
+            selectedCategory: "All",
+            selectedSeller: "All",
+        }
+    },
+    methods: {
+        updateProductCategory(event, productFilterID){
+            this.$parent.filterProductBy(event, productFilterID);
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .productFilter {  
+        text-align: start;
+    }
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,30 +1,10 @@
 <template>
     <div class="products">
         <div class="row">
-        <div class="col-md-3 order-md-1 mb-4 filters">
+        <div class="col-md-3 order-md-1 mb-4">
 
-          <h4 class="d-flex mb-3">
-            <span class="text-muted">Filters: </span>
-          </h4>
-          <form>
-            <div class="form-group">
-            <label for="exampleFormControlSelect1">By Category:</label>
-            <select class="form-control" id="exampleFormControlSelect1" v-model="selectedCategory" @change="filterProductByCategory">
-              <option 
-              v-for="(category, index) in categories" :key="index" 
-              v-bind:value="category.productCategory" 
-              >{{category.productCategory}}</option>
-            </select>
-          </div>
-          </form>
-          <ul class="list-group">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-          </ul>
-
+          <product-filter :categories="categories" :sellers="sellers"/>
+          
         </div>
         <div class="col-md-9 order-md-2">
           <!-- <h4 class="mb-3">Billing address</h4> -->
@@ -40,18 +20,19 @@
 <script>
 import ProductsList from "@/components/products/ProductsList";
 import CardLoader from "@/components/shared/CardLoader";
+import ProductFilter from "@/components/products/filters/ProductFilter"
 import axios from "axios";
 import _ from "lodash";
 
 export default {
   name: "allProducts",
-  components: { ProductsList, CardLoader },
+  components: { ProductsList, CardLoader, ProductFilter },
   data() {
     return {
       categories: [],
+      sellers: [],
       products_list: [],
       product_duplicate: [],
-      selectedCategory: "All",
       loading: false
     };
   },
@@ -73,18 +54,26 @@ export default {
             "productCategory"
           );
           this.categories.unshift({ productCategory: "All" });
+          
+          this.sellers = _.uniqBy(
+            _.map(this.products_list, function(object) {
+              return _.pick(object, ["productSeller"]);
+            }),
+            "productSeller"
+          );
+          this.sellers.unshift({ productSeller: "All" });
         })
         .catch(error => {
           console.log(error);
         });
     },
     
-    filterProductByCategory(event) {
+    filterProductBy(event, productFilterID) {
       const productList = [...this.products_list];
       let filteredData = [];
       if (event.target.value !== "All") {
         productList.map(product => {
-          if (product.productCategory === event.target.value) {
+          if (product[productFilterID] === event.target.value) {
             filteredData.push(product);
           }
         });
@@ -106,8 +95,5 @@ export default {
 <style lang="scss">
 .products {
   margin-top: 40px;
-  .filters {
-    text-align: start;
-  }
 }
 </style>
