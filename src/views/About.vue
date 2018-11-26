89d402e2c5d513821767af53f15f9f54b36e09af
ikismail <ikismail7@gmail.com>
2018-11-26 21:44:00 +0530

    Updated About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,33 +2,45 @@
   <div class="about">
     <div class="container">
       <div class="modal-content text-center">
-        <div class="modal-header ">
-          <h4 class="modal-title" style="margin: 0 auto">More About
+        <div class="modal-header">
+          <h4 class="modal-title" style="margin: 0 auto">
+            More About
             <span style="color:crimson">ikismail</span>
           </h4>
         </div>
         <div class="modal-body">
-          <img src="https://avatars1.githubusercontent.com/u/17190913?s=460&v=4" name="aboutme" width="140" height="140" border="0" class="avatar">
-          <h3 class="media-heading">Mohammed Ismail
-          </h3>
+          <img
+            src="https://avatars1.githubusercontent.com/u/17190913?s=460&v=4"
+            name="aboutme"
+            width="140"
+            height="140"
+            border="0"
+            class="avatar"
+          >
+          <h3 class="media-heading">Mohammed Ismail</h3>
           <h6>
             <strong>ikismail7@gmail.com</strong>
           </h6>
+          <h6>
+            <a href="https://ikismail.github.io/" target="_blank">ikismail.github.io</a>
+            <strong></strong>
+          </h6>
 
-          <span class="badge  badge-warning p-2 m-1">HTML5/CSS</span>
-          <span class="badge  badge-info p-2 m-1">Adobe CS 5.5</span>
-          <span class="badge  badge-info p-2 m-1">Microsoft Office</span>
-          <span class="badge  badge-success p-2 m-1">Windows XP, Vista, 7</span>
-          <hr>
-          <p class="text-left">
-            <strong>Bio: </strong>
-            <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.
+          <span class="badge badge-warning p-2 m-1">Angular</span>
+          <span class="badge badge-primary p-2 m-1">Vuejs</span>
+          <span class="badge badge-secondary p-2 m-1">React</span>
+          <span class="badge badge-success p-2 m-1">Github</span>
+          <span class="badge badge-danger p-2 m-1">RestService</span>
+          <!-- <hr> -->
+          <!-- <p class="text-left">
+            <strong>Bio:</strong>
+            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.
           </p>
-          <br>
+          <br>-->
         </div>
-        <div class="modal-footer" style="margin: 0 auto">
+        <!-- <div class="modal-footer" style="margin: 0 auto">
           <button type="button" class="btn btn-default" data-dismiss="modal">Read More</button>
-        </div>
+        </div>-->
       </div>
     </div>
   </div>
